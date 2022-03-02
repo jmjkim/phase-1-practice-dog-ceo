e0762d093b39d6b71c0432b60a01d0b4cefaf0de
Jongmin Jack Kim <jmjkim00@icloud.com>
2022-03-01 18:14:37 -0800

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -5,65 +5,62 @@ document.addEventListener('DOMContentLoaded', () => {
     const imgUrl = 'https://dog.ceo/api/breeds/image/random/4';
     const breedUrl ='https://dog.ceo/api/breeds/list/all';
 
-    function challengeOne() {
-    fetch(imgUrl)
-        .then(res => res.json())
-        .then(data => data.message.forEach(url => {
+    // Fetching JSON data from a given URL and executes a callback function.
+    function fetchJson(url, callback) {
+        fetch(url)
+            .then(res => res.json())
+            .then(callback)
+            .catch(err => console.log(err));
+    } 
+
+
+    // Challenge 1 
+    function displayImages(data) {
+        data.message.forEach(url => {
             const img = document.createElement('img');
             img.setAttribute('src', url);
             document.getElementById('dog-image-container').appendChild(img);
-        }))
-        .catch(err => console.log(err));
-    } 
-    // challengeOne()
+        })
+    }
+    fetchJson(imgUrl, displayImages)
 
 
+    // Challenge 2
+    function displayBreeds(data) {
+        for ([key, value] of Object.entries(data.message)) {
+            const li = document.createElement('li');
+
+            li.innerText = `${key}: ${value}`;
+            document.getElementById('dog-breeds').appendChild(li);
+
+            // Challenge 3 
+            li.addEventListener('click', () => {
+                li.classList.toggle('liColor');
+            })
+        }
+    }
+    fetchJson(breedUrl, displayBreeds)
 
 
-    function challengeTwo() {
-        fetch(breedUrl)
-            .then(res => res.json())
-            .then(data => {
-            
-                for ([key, value] of Object.entries(data.message)) {
+    // Challenge 4
+    function dropdownSelection(data) {
+        const dropdown = document.getElementById('breed-dropdown');
+        const ul = document.getElementById('dog-breeds')
+        dropdown.addEventListener('change', e => {
+            ul.innerHTML = '';
+
+            const optionValue = e.target.value;
+            Object.keys(data.message).forEach(element => {
+                const firstLetter = element.charAt(0);
+                
+                if (optionValue === firstLetter) {
                     const li = document.createElement('li');
-                    li.innerText = `${key}: ${value}`;
+                    li.textContent = element;
                     document.getElementById('dog-breeds').appendChild(li);
-
-                    // Challenge 3 
-                    li.addEventListener('click', e => e.target.setAttribute('style', 'color: blue;'));
-                }
+                }})
             })
-            .catch(err => console.log(err));
         }
-        // challengeTwo()
-
-
-
-
-    function challengeFour() {
-        fetch(breedUrl)
-            .then(res => res.json())
-            .then(data => {
-                const dropdown = document.getElementById('breed-dropdown');
-
-                dropdown.addEventListener('change', e => {
-                    const optionValue = e.target.value;
-                    
-                    Object.keys(data.message).forEach(element => {
-                        const firstLetter = element.charAt(0);
-        
-                        if (optionValue === firstLetter) {
-                            const li = document.createElement('li');
-                            li.textContent = element;
-                            document.getElementById('dog-breeds').appendChild(li);
-                        }})
-                    })
-            })
-            .catch(err => console.log(err));
-        }
-        challengeFour()
-
+        fetchJson(breedUrl, dropdownSelection)
     })
         
 
